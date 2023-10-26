a55731b1965ad6faec41fd2d02378508f48e485e
Xiao <2085127827@qq.com>
2023-10-26 13:14:01 +0800

    test

--- mindspore_object_detection/ssd/src/__init__.py
+++ mindspore_object_detection/ssd/src/__init__.py
@@ -1,4 +1,5 @@
 '''初始化'''
+# pylint: disable=E0401
 from .res50_backbone import resnet50
 from .ssd_model import SSD300, Backbone
 from .utils import dboxes300_coco, calc_iou_tensor, Encoder, PostProcess
